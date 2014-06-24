fd1cff4ba7def8543fe01c5d845e0bee5c273851
ANDROID-8 <android.88@list.ru>
2014-06-24 11:29:42 +0300

    add dz10

---
+++ dz10/dz10_3_1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,rez;
+    printf("A=");
+    scanf("%d",&a);
+    printf("B=");
+    scanf("%d",&b);
+    printf("C=");
+    scanf("%d",&c);
+    rez=a+b+c;
+    printf("A+B+C=%d\n",rez);
+    rez=a-b-c;
+    printf("A-B-C=%d\n",rez);
+    rez=a*b*c;
+    printf("A*B*C=%d\n",rez);
+    if (b==0 || c==0)
+    {
+        printf("A/B/C= ERROR! Division by zero.");
+    }
+    else
+    {
+        rez=a/b/c;
+        printf("A/B/C=%d\n",rez);
+        double rez=(double)a/b/c;
+        printf("A/B/C=%.3f\n",rez);
+    }
+    return 0;
+}
---
+++ dz10/dz10_3_2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,x,s;
+    printf("N=");
+    scanf("%d",&n);
+    s=0;
+    for (i=1; i<=n; ++i)
+    {
+        printf("x[%d]=",i);
+        scanf("%d",&x);
+        s=s+x;
+    }
+    if (s>100)
+    {
+        printf("Your sum of numbers >100");
+    }
+    else
+    {
+        if (s<100)
+        {
+            printf("Your sum of numbers <100");
+        }
+        else
+        {
+            printf("sum=%d",s);
+        }
+    }
+    return 0;
+}
