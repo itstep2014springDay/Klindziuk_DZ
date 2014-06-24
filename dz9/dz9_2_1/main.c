ec40add4c3c344f7f406f45d2fd59ed8a7463a1f
ANDROID-8 <android.88@list.ru>
2014-06-24 12:38:32 +0300

    update ёлка

--- dz9/dz9_2_1/main.c
+++ dz9/dz9_2_1/main.c
@@ -18,7 +18,14 @@ int main()
             }
             for (int j=1; j<=2*i-1; ++j)
             {
-                printf("#");
+                if (j==1 || i==k+1 || j==2*i-1)
+                {
+                    printf("#");
+                }
+                else
+                {
+                    printf(" ");
+                }
             }
             printf("\n");
         }
