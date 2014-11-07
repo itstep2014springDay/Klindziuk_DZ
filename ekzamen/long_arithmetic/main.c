752185cf25717ac151bf091aafa365fcf8324a87
ANDROID-8 <android.88@list.ru>
2014-11-08 00:47:42 +0300

    ekzamen: read long

--- ekzamen/long_arithmetic/main.c
+++ ekzamen/long_arithmetic/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAXDIGITS 1000
+#define BASE 10000
+
+typedef int myLongType[MAXDIGITS];
+
+void readLong(myLongType a)
+{
+    memset(a,0,sizeof(myLongType));
+    char current=getchar();
+    while (current!='\n')
+    {
+        for (int i=a[0];i>=1;--i)
+        {
+            a[i+1]=a[i+1]+a[i]*10/BASE;
+            a[i]=a[i]*10%BASE;
+        }
+        a[1]=a[1]+current-'0';
+        if (a[a[0]+1]>0)
+            ++a[0];
+        current=getchar();
+    }
+}
+
 
 int main()
 {
+    myLongType a,b;
+    readLong(a);
+
     printf("Hello world!\n");
     return 0;
 }
