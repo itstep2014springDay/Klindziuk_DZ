fca658fc13183024e157fdd93af85cc454e5e042
ANDROID-8 <android.88@list.ru>
2014-11-08 01:55:58 +0300

    ekzamen: write long

--- ekzamen/long_arithmetic/main.c
+++ ekzamen/long_arithmetic/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MAXDIGITS 1000
 #define BASE 10000
@@ -25,11 +26,32 @@ void readLong(myLongType a)
     }
 }
 
+void writeLong(myLongType a)
+{
+    //char ls[10],s[10];
+    char ls[(int) log10(MAXDIGITS)],s[(int) log10(MAXDIGITS)];
+    sprintf(ls,"%d",BASE/10);
+    printf("%d",a[a[0]]);
+    for(int i=a[0]-1;i>=1;--i)
+    {
+        sprintf(s,"%d",a[i]);
+        while (strlen(s)<strlen(ls))
+        {
+            char tmpStr[10]="0";
+            strcat(tmpStr,s);
+            strcpy(s,tmpStr);
+        }
+        printf("%s",s);
+    }
+    printf("\n");
+}
 
 int main()
 {
     myLongType a,b;
     readLong(a);
+
+    writeLong(a);
 
     printf("Hello world!\n");
     return 0;
