f7c32d058a3535cca6b2fdc3336e187e3695e32f
ANDROID-8 <android.88@list.ru>
2014-11-08 20:15:00 +0300

    ekzamen: sumLong

--- ekzamen/long_arithmetic/main.c
+++ ekzamen/long_arithmetic/main.c
@@ -14,7 +14,7 @@ void readLong(myLongType a)
     char current=getchar();
     while (current!='\n')
     {
-        for (int i=a[0];i>=1;--i)
+        for (int i=a[0]; i>=1; --i)
         {
             a[i+1]=a[i+1]+a[i]*10/BASE;
             a[i]=a[i]*10%BASE;
@@ -26,16 +26,16 @@ void readLong(myLongType a)
     }
 }
 
-void writeLong(myLongType a)
+void writeLong(const myLongType a)
 {
     //char ls[10],s[10];
     char ls[(int) log10(MAXDIGITS)],s[(int) log10(MAXDIGITS)];
     sprintf(ls,"%d",BASE/10);
     printf("%d",a[a[0]]);
-    for(int i=a[0]-1;i>=1;--i)
+    for(int i=a[0]-1; i>=1; --i)
     {
         sprintf(s,"%d",a[i]);
-        while (strlen(s)<strlen(ls))
+        while (strlen(s)<strlen(ls)) // а в паскале тут всего одна строка  s:='0'+s;
         {
             char tmpStr[10]="0";
             strcat(tmpStr,s);
@@ -46,13 +46,36 @@ void writeLong(myLongType a)
     printf("\n");
 }
 
+void sumLong(const myLongType a, const myLongType b, myLongType c)
+{
+    memset(c,0,sizeof(myLongType));
+    int k;
+    if (a[0]>b[0])
+        k=a[0];
+    else
+        k=b[0];
+    for (int i=1; i<=k; ++i)
+    {
+        c[i+1]=(c[i]+a[i]+b[i])/BASE;
+        c[i]=(c[i]+a[i]+b[i])%BASE;
+    }
+    if (c[k+1]==0)
+        c[0]=k;
+    else
+        c[0]=k+1;
+}
+
 int main()
 {
-    myLongType a,b;
+    myLongType a,b,c;
+    printf("A= ");
     readLong(a);
+    printf("B= ");
+    readLong(b);
 
-    writeLong(a);
+    sumLong(a,b,c);
+    printf("C= ");
+    writeLong(c);
 
-    printf("Hello world!\n");
     return 0;
 }
